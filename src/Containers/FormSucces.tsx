ca89a32c7394ec034449a1dac10ae8bad7f48c1e
Sergio Cortes <sergiocortessat@gmail.com>
2021-11-02 08:54:34 +0100

    Add style form succes

--- src/Containers/FormSucces.tsx
+++ src/Containers/FormSucces.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const FormSucces = () => {
     return (
         <div className="form-content-right">
-            <div className="from-success">We have received your request!</div>
+            <div className="form-success">We have received your request!</div>
             <img src="./img/img-3.svg" alt="success" className="form-img-2" />
         </div>
     )
